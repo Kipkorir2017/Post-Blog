2089d8a933171616759a8d208666d21fae75c357
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-24 18:31:43 +0300

    update config file and move to the root folder and also update the init file

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    
+    pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
