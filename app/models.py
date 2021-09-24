4d72b2c6100a36e3235b69bf6844fe48e06dd77e
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-24 08:42:56 +0300

    add models class in the models file

---
+++ app/models.py
@@ -0,0 +1,30 @@
+
+
+
+class User():
+    '''
+    User class to define user Objects
+
+    '''
+
+
+
+class  Blog():
+    '''
+    Blog class to define Blog objects
+    '''
+
+
+    
+
+class Subscriber():
+    '''
+    Subscriber model class  to def subscribers objects
+    '''
+
+
+
+class Quote():
+    '''
+    model class for quotes
+    '''
