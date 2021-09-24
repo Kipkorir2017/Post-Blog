6ad7fd91c57a02f9061932baeb94d535f5670eb5
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-24 18:22:01 +0300

    add changes to templates files and view,request files

--- app/request.py
+++ app/request.py
@@ -1,4 +1,15 @@
 from flask import request
 from app import app
+from .models import Quote
+
+
+def get_quote():
+    """
+    Function to take http request and return an instance of a Quote
+    """
+    response = request.get("http://quotes.stormconsultancy.co.uk/random.json").json()
+
+    random_quote = Quote(response.get("author"), response.get("quote"))
+    return random_quote
 
 
--- app/views.py
+++ app/views.py
@@ -1,13 +1,17 @@
 from app import app
 from flask import render_template
+from .models import Blog
+from request import get_quote
 
 
-
-@app.route('/')
+@app.route("/")
 def index():
     '''
     view function to return index page
 
     '''
+    blogs = Blog.get_all_blogs()
+    quote = get_quote()
+
     title="Post-Blog"
-    return render_template('index.html', head=title)
+    return render_template("index.html",blogs=blogs,quote=quote,head=title)
