c789370e86fc82cf328cced8d2aac6c7a8f24c52
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-25 11:36:24 +0300

    add auth folder and its files

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,11 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    # setting config
+    
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     # from .request import configure_request
     # configure_request(app)
 
